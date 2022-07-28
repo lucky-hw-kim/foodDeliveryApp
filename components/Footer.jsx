183df131e6976d644945dc2b660150b2f27c60f6
lucky <lucky.hw.kim@gmail.com>
2022-07-28 15:09:22 -0700

    main page

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,21 @@
+import {UilFacebook, UilGithub, UilInstagram} from '@iconscout/react-unicons'
+import Image from 'next/image'
+import css from '../styles/Footer.module.css'
+import Logo from '../assets/Logo.png'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={css.container}>
+      <span>ALL RIGHT RESERVED</span>
+      <div className={css.social}>
+        <UilFacebook size={45}/>
+        <UilGithub size={45}/>
+        <UilInstagram size={45}/>
+      </div>
+      <div className={css.logo}>
+      <Image src={Logo} alt="Logo" width="50" height="50"/>
+      <span>Fudo</span>
+    </div>
+    </div>
   )
 }
 
---
+++ components/Hero.jsx
@@ -0,0 +1,49 @@
+import css from "../styles/Hero.module.css";
+import Cherry from "../assets/Cherry.png";
+import HeroImage from "../assets/HeroImage.png";
+import Image from "next/image";
+import {UilPhone} from '@iconscout/react-unicons'
+
+const Hero = () => {
+  return (
+    <div className={css.container}>
+      {/* left side */}
+      <div className={css.left}>
+        <div className={css.cherryDiv}>
+          <span>Fast Delivery</span>
+          <Image src={Cherry} alt="cherry" height={25} width={40} />
+        </div>
+
+        <div className={css.heroText}>
+          <span>Be The Fastest</span>
+          <span>In Delivering</span>
+          <span> Your </span>
+          <span style={{ color: "var(--themeRed)" }}>Pizza</span>
+        </div>
+        <span className={css.miniText}>
+          Our mission is to filling your tummy with delicious food and free Delivery
+        </span>
+        <button className={`btn ${css.btn}`}>
+          Get Started
+        </button>
+
+
+      </div>
+
+      {/* right side */}
+      <div className={css.right}>
+        <div className={css.imageContainer}>
+          <Image src={HeroImage} alt="heroimage" layout="intrinsic"/>
+        </div>
+        <div className={css.ContactUs}>
+          <span>Contact Us</span>
+          <div>
+            <UilPhone color="white"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
